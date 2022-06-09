469ec0483af504d31eeb94034d6fb69f26509614
Dmitriy Chernov <chernov.idm@gmail.com>
2022-06-10 00:30:00 +0300

    Добавлены дефолтные значения в prompt

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let title = prompt('Как называется наш проект?');
+let title = prompt('Как называется наш проект?', "РасЧет стоИмости РаБот");
 let screens = prompt('Какие типы экранов разработать?', "Простые, Сложные, Интерактивные");
 let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = 25;
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?', "РазРабоТка дизайна");
 let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?', "ВеРстКа");
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 let adaptive = confirm('Нужен ли адаптив на сайте?');
 
 let servicePercentPrice = Math.ceil(fullPrice - (fullPrice/100*rollback));
-console.log(servicePercentPrice);
+console.log(fullPrice);
 
 if (fullPrice >= 30000) {
     console.log('Даем скидку в 10%');
@@ -23,12 +23,9 @@ if (fullPrice >= 30000) {
     console.log('Что-то пошло не так');
 }
 
-
-
-
-// console.log(typeof title, typeof fullPrice, typeof adaptive);
-// console.log(screens.length);
-// console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
-// console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
-// console.log(screens.toLowerCase().split(", "));
-// console.log(fullPrice * (rollback/100));
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback/100));
